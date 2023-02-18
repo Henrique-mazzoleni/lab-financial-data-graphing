ad5112102a0e991d581427558bf20edd58b3c84b
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-02-18 15:20:51 +0100

    lab solved

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,15 +1,50 @@
 const apiUrl = "https://api.coindesk.com/v1/bpi/historical/close.json";
 
+const [startDateInput] = document.getElementsByName('startDate')
+const [endDateInput] = document.getElementsByName('endDate')
+const currencyInput = document.getElementById('currency')
+const maxValue = document.getElementById('maxValue')
+const minValue = document.getElementById('minValue')
+const now = new Date();
+startDateInput.value = '2015-01-01'
+endDateInput.valueAsDate = new Date()
+
 const fetchData = async () => {
-  const apiUrl = "https://api.coindesk.com/v1/bpi/historical/close.json";
+  const response = await fetch(`${apiUrl}?start=${startDateInput.value}&end=${endDateInput.value}&currency=${currencyInput.value}`);
+  const rawData = await response.json();
+    
+  maxValue.innerHTML = `${Math.max(...Object.values(rawData.bpi)).toFixed(2)} ${currencyInput.value}`
+  minValue.innerHTML = `${Math.min(...Object.values(rawData.bpi)).toFixed(2)} ${currencyInput.value}`
 
-  const now = new Date();
-  const nowParsed = `${now.getFullYear()}-${now.getMonth().toString().padStart(2, '0')}-${now.getDay().toString().padStart(2, '0')}`;
-  const oneYearAgoParsed = `${now.getFullYear() - 1}-${now.getMonth().toString().padStart(2, '0')}-${now.getDay().toString().padStart(2, '0')}`;
-  const response = await fetch(`${apiUrl}?start=${oneYearAgoParsed}&end=${nowParsed}`);
-  const data = await response.json();
-
-  console.log(data);
+  return Object.entries(rawData.bpi).map(entry => ({date: entry[0], value: entry[1]}));
 };
 
-fetchData();
+const canvas = document.getElementById('myChart')
+let graph;
+
+const drawGraph = async () => {
+    const data = await fetchData();
+
+    
+    if (graph) graph.destroy();
+    graph = new Chart(canvas, {
+        type: 'line',
+        data: {
+            labels: data.map(row => row.date),
+            datasets: [
+                {
+                    label: 'Bitcoin Price Index',
+                    data: data.map(row => row.value)
+                }
+            ]
+        }
+    })
+}
+
+
+startDateInput.addEventListener('change', drawGraph)
+endDateInput.addEventListener('change', drawGraph)
+currencyInput.addEventListener('change', drawGraph)
+
+drawGraph()
+
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const moment = require('moment')
 
 /* GET home page */
 router.get("/", async (req, res, next) => {
-  
   res.render("index");
 });
 
